831b5e9586875790899806cfd06fb63d2d4c23cd
Victor Penso <v.penso@gsi.de>
2018-05-29 13:33:32 +0200

    support VM instance re-configure

--- var/aliases/vm.sh
+++ var/aliases/vm.sh
@@ -33,6 +33,7 @@ vm <command>
  lo, login <name> <args>      login into an instance
  lk, lookup <name>            show network configuration tuple
  r , remove <name>            delete an instance
+ re, redefine <name>          re-define instance after configuration change
  p , path <name>              print path to an instance
  s , shadow <image> <name>    shadow image, and start instance
  sh, shutdown <name>          graceful shutdown instance        
@@ -55,35 +56,53 @@ function vm() {
     name=${1:?Expecting a virtual machine instance name as argument!}
     cd $(virsh-instance path $name)
     ;;
-  "clone"|"cl")                virsh-instance clone $@ ;;
-  "create"|"c")                virsh create $@ ;;
-  "config"|"co")               
+  clone|cl)                virsh-instance clone $@ ;;
+  create|c)                virsh create $@ ;;
+  config|co)               
     vm cd $1
     shift
     virsh-config $@ 
     cd - >/dev/null
     ;;
-  "define"|"d")                virsh define $@ ;;
-  "image"|"i")                 virsh-instance image ;;
-  "ip")                        virsh-nat-bridge lookup $1 | cut -d' ' -f2 ;;
-  "kill"|"k")        
+  define|d)                virsh define $@ ;;
+  image|i)                 virsh-instance image ;;
+  ip)
+    virsh-nat-bridge lookup $1 | cut -d' ' -f2 
+    ;;
+  kill|k)
     virsh undefine $(virsh-instance fqdn $1)
     virsh destroy $(virsh-instance fqdn $1)
     ;;
-  "list"|"l")                  virsh list --all | tail -n +3 | sed '/^$/d' | tr -s ' ' | cut -d' ' -f3- ;;
-  "login"|"lo"|"exec"|"ex")    vm cd $1 ; shift ; ssh-instance $@ ; cd - >/dev/null;;
-  "lookup"|"lk")               virsh-nat-bridge lookup $@ ;;
-  "mount"|"m")                 vm cd $1 ; shift ; sshfs-instance mount $@ ; cd - >/dev/null;;
-  "nat"|"n")                   virsh-nat-bridge $@ ;;
-  "path"|"p")                  virsh-instance path $@ ;;
-  "remove"|"r")                virsh-instance remove $@ ;;
-  "shutdown"|"sh")             virsh shutdown $(virsh-instance fqdn $1) ;;
-  "shadow"|"s")                virsh-instance shadow $@ ;;
-  "start"|"st")                virsh start $(virsh-instance fqdn $1) ;;
-  "sync"|"sy")                 vm cd $1 ; shift ; rsync-instance $@ ; cd - >/dev/null ;;
-  "umount"|"um")               vm cd $1 ; sshfs-instance umount ; cd - >/dev/null ;;
-  "undefine"|"u")              virsh undefine $(virsh-instance fqdn $1) ;;
-  *) 
+  list|l)
+    virsh list --all | tail -n +3 | sed '/^$/d' | tr -s ' ' | cut -d' ' -f3- 
+    ;;
+  login|lo|exec|ex)
+    vm cd $1
+    shift
+    ssh-instance $@
+    cd - >/dev/null
+    ;;
+  lookup|lk)               virsh-nat-bridge lookup $@ ;;
+  mount|m)                 vm cd $1 ; shift ; sshfs-instance mount $@ ; cd - >/dev/null;;
+  nat|n)                   virsh-nat-bridge $@ ;;
+  path|p)                  virsh-instance path $@ ;;
+  remove|r)                virsh-instance remove $@ ;;
+  redefine|re)
+    virsh undefine $(virsh-instance fqdn $1)
+    virsh define $(virsh-instance path $name)/libvirt_instance.xml
+    ;;
+  shutdown|sh)             virsh shutdown $(virsh-instance fqdn $1) ;;
+  shadow|s)                virsh-instance shadow $@ ;;
+  start|st)                virsh start $(virsh-instance fqdn $1) ;;
+  sync|sy)
+    vm cd $1
+    shift
+    rsync-instance $@
+    cd - >/dev/null 
+    ;;
+  umount|um)               vm cd $1 ; sshfs-instance umount ; cd - >/dev/null ;;
+  undefine|u)              virsh undefine $(virsh-instance fqdn $1) ;;
+  *)
     echo "$VM_FUNCTION_HELP"
     ;;
   esac
